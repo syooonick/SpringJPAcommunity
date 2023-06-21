84b01206df59d37582b0b9c6e915cd1f8c254afc
Sung Yoon Lee <127701757+syooonick@users.noreply.github.com>
2023-06-21 20:25:04 +0900

    COMMUNITY

---
+++ src/main/java/com/study/board/web/dto/auth/SignupDto.java
@@ -0,0 +1,21 @@
+package com.study.board.web.dto.auth;
+
+import com.study.board.entity.User;
+import lombok.Data;
+
+@Data
+public class signupDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .name(name)
+                .build();
+    }
+}
